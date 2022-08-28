1a5722de47431777bfcb34d229a87735de301c54
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2022-08-27 21:37:04 -0300

    Implemented knockback on BasicEnemy when hit by player attacks.

--- Assets/Sandbox/PedroA/Scripts/Entities/Enemies/BasicEnemy/States/BasicEnemyDamageState.cs
+++ Assets/Sandbox/PedroA/Scripts/Entities/Enemies/BasicEnemy/States/BasicEnemyDamageState.cs
@@ -20,6 +20,14 @@ namespace Tortoise.HOPPER
             _BasicEnemy.Agent.SetDestination(_BasicEnemy.transform.position);
             _BasicEnemy.Agent.isStopped = true;
 
+            _BasicEnemy.Agent.updatePosition = false;
+            _BasicEnemy.Rigidbody.isKinematic = false;
+
+            var direction = _BasicEnemy.transform.position - _BasicEnemy.Target.position;
+            direction.y = 0f;
+            _BasicEnemy.Rigidbody.AddForce(direction.normalized * _BasicEnemy.Data.KnockbackForce, ForceMode.VelocityChange);
+
+            _BasicEnemy.AnimationHelper.SetRootMotion(true);
 
             _BasicEnemy.AnimationHelper.SetAnimationBool(_BasicEnemy.AnimationData.MovingParamHash, false);
             _BasicEnemy.AnimationHelper.SetAnimationBool(_BasicEnemy.AnimationData.AttackingParamHash, false);
@@ -29,6 +37,10 @@ namespace Tortoise.HOPPER
         {
             base.AnimationExitEvent();
 
+            _BasicEnemy.Rigidbody.isKinematic = true;
+
+            _BasicEnemy.Agent.nextPosition = _BasicEnemy.transform.position;
+            _BasicEnemy.Agent.updatePosition = true;
             _BasicEnemy.Agent.isStopped = false;
 
             _StateMachine.CurrentCooldown = _BasicEnemy.Data.DamageCooldown;
